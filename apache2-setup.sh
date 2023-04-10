e1fa4ddaf2d11dd42340c8e50d90bc84bf4442b5
Josh Vejendla <joshvejendla@gmail.com>
2023-04-10 07:39:52 -0700

    Update apache2-setup.sh

--- apache2-setup.sh
+++ apache2-setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-// Switch into root user
+# Switch into root user
 Sudo su
 
-// Download Apache 2
+# Download Apache 2
 apt-get install apache2 apacheutils
 
-// Start and enable the Apache 2 service
+# Start and enable the Apache 2 service
+servicectl restart apache2
+servicectl enable apache2
 
+# Go to firefox and type the word "localhost" into the search bar
